490bafcd729dab67b06d95f27f9802fd2fb039af
abayomi igwubor <abayomi.igwubor@libmot.com>
2021-03-19 07:42:23 +0100

    added useeffect

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import GlobalFeed from "pages/globalFeed";
 import Article from "pages/article";
+import Authentication from "pages/authentication";
 
 const Routes = () => {
   return (
     <Switch>
       <Route path="/" component={GlobalFeed} exact />
       <Route path="/articles/:slug" component={Article} />
+      <Route path="/login" component={Authentication} />
     </Switch>
   );
 };
